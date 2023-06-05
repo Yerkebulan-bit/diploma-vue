90cf7372da042a452a57243542ee3e61492c18f2
amanispaltieva <paltieva_02@mail.ru>
2023-06-05 06:29:42 +0600

    feat: add main logic

--- src/components/select-component/select-component.vue
+++ src/components/select-component/select-component.vue
@@ -1,37 +1,39 @@
-<script setup lang="ts">
-import type { PropType } from 'vue'
-import type {WeekToSelect} from "@/domain/interfaces/WeekToSelect.interace";
+<!--<script setup lang="ts">-->
+<!--import type { PropType } from 'vue'-->
+<!--import type {WeekToSelect} from "@/domain/interfaces/WeekToSelect.interace";-->
 
-defineProps({
-  selectedItem: Object as PropType<WeekToSelect>,
-  items: Array as PropType<WeekToSelect[]>
-})
-</script>
+<!--defineProps({-->
+<!--  selectedItem: Object as PropType<WeekToSelect>,-->
+<!--  items: Array as PropType<WeekToSelect[]>-->
+<!--})-->
 
-<template>
-  <div class="select-component">
-    <div class="select-component__header">
-      <div class="select-component__title">{{ selectedItem?.name }}</div>
-      <div class="select-component__close">
-      </div>
-    </div>
-    <div class="select-component__content">
-      <ul class="select-component__list">
-        <li
-          class="select-component__item"
-          v-for="item in items"
-          :key="item.id"
-          @click="$emit('select', item.value)"
-        >
-          <div class="select-component__small-title">
-            {{ item.name }}
-          </div>
-        </li>
-      </ul>
-    </div>
-  </div>
-</template>
+<!--</script>-->
 
-<style scoped lang="scss">
-@import "select-component";
-</style>
+<!--<template>-->
+<!--  <div class="select-component">-->
+<!--    <div class="select-component__header">-->
+<!--      <div class="select-component__title" @>{{ selectedItem?.name }}</div>-->
+<!--      <div class="select-component__close">-->
+<!--      </div>-->
+<!--    </div>-->
+<!--    <div class="select-component__content">-->
+<!--      <ul class="select-component__list">-->
+<!--        <li-->
+<!--          class="select-component__item"-->
+<!--          v-for="item in items"-->
+<!--          :key="item.id"-->
+<!--          @click="$emit('select', item.value)"-->
+<!--        >-->
+<!--          <div class="select-component__small-title">-->
+<!--            {{ item.name }}-->
+<!--          </div>-->
+<!--        </li>-->
+<!--      </ul>-->
+<!--    </div>-->
+<!--  </div>-->
+<!--</template>-->
+
+<!--<style scoped lang="scss">-->
+<!--@import "select-component";-->
+<!--</style>-->
+<template></template>
